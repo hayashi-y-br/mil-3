0f7a7853ec3c36203cc8d4ed3772d60806e67a48
hayashi-y-br <hayashi.y.br@m.titech.ac.jp>
2024-07-18 14:28:47 +0900

    Add visualization.py

---
+++ visualization.py
@@ -0,0 +1,41 @@
+import numpy as np
+import torch
+import torchvision
+from torchvision.utils import make_grid
+import matplotlib.pyplot as plt
+
+for i in range(3):
+    img_list = []
+    for j in range(10):
+        img_list.append(plt.imread(f'./img/attention/img_{i}_{j}.png'))
+    img = np.stack(img_list)
+    img = torch.from_numpy(img)
+    img = torch.permute(img, (0, 3, 1, 2))
+    img = torchvision.utils.make_grid(img, nrow=5, padding=0)
+    img = torch.permute(img, (1, 2, 0))
+
+    fig, ax = plt.subplots(figsize=(18, 8))
+    ax.axis('off')
+    ax.imshow(img)
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    fig.savefig(f'./visualization/attention/visualization_{i}')
+    plt.close(fig)
+
+
+for i in range(3):
+    for k in range(3):
+        img_list = []
+        for j in range(10):
+            img_list.append(plt.imread(f'./img/additive/img_{i}_{j}_{k}.png'))
+        img = np.stack(img_list)
+        img = torch.from_numpy(img)
+        img = torch.permute(img, (0, 3, 1, 2))
+        img = torchvision.utils.make_grid(img, nrow=5, padding=0)
+        img = torch.permute(img, (1, 2, 0))
+
+        fig, ax = plt.subplots(figsize=(18, 8))
+        ax.axis('off')
+        ax.imshow(img)
+        fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+        fig.savefig(f'./visualization/additive/visualization_{i}_{k}')
+        plt.close(fig)
